565cfd8168a923ed614c147f12c345cd1344451d
影月 零 <zero@genshin.org>
2014-10-03 21:08:40 +0900

    add linter

---
+++ util/inkscape_lint.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+#########################################
+# inkscape_lint
+#
+# uses inkscape to re-save SVG images
+# cleaning out dirty/non-standard
+# adobe etc. namespaces
+#########################################
+
+require 'rubygems'
+
+gem 'emojidex'
+
+require 'fileutils'
+require 'emojidex'
+
+emoji_root = File.expand_path('../../emoji/', __FILE__)
+utf_path = File.expand_path('utf', emoji_root)
+extended_path = File.expand_path('extended', emoji_root)
+
+utf = Emojidex::Collection.new
+utf.load_local_collection utf_path
+
+extended = Emojidex::Collection.new
+extended.load_local_collection extended_path
+
+utf.each do |moji|
+  `inkscape --vacuum-defs #{utf_path}/#{moji.code}.svg`
+end
